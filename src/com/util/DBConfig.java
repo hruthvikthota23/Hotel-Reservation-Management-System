c25c0593cf293cc9e7e4f3755b9c8a6a046a0ce6
hruthvikthota23 <sunnynetha23@gmail.com>
2026-09-14 23:47:27 +0530

    Remove database credentials from repository

--- src/com/util/DBConfig.java
+++
@@ -1,7 +0,0 @@
-package com.util;
-
-public class DBConfig {
-    public static final String URL="jdbc:mysql://localhost:3306/hotel_reservation";
-    public static final String USERNAME="root";
-    public static final String PASSWORD="your_password";
-}
